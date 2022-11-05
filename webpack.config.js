6ed37dacf5c157ba188b72b380145b9f9717f74a
Еропкин Кирилл <krill2211@yandex.ru>
2022-11-05 23:13:05 +0300

    Added: compression

--- webpack.config.js
+++ webpack.config.js
@@ -40,19 +40,7 @@ const config = {
         },
       ],
     }),
-    new CompressionPlugin({
-      filename: "[path][base].br",
-      algorithm: "brotliCompress",
-      test: /\.(js|css|html|svg|webp)$/,
-      compressionOptions: {
-        params: {
-          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-        },
-      },
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: true,
-    }),
+    new CompressionPlugin(),
   ],
   module: {
     rules: [
